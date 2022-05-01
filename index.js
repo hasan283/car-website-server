9671baa5c37ff592ce5011c57a12b7dd909645d5
mdhasan-999 <webdeveloperhasan999@gmail.com>
2022-05-01 10:25:38 +0600

    Delete, Add Item and Manage Item

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -17,7 +17,46 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try {
+        await client.connect();
+        const stockCollection = client.db('inventoryCar').collection('stock');
+        app.get('/stock', async (req, res) => {
+            const query = {};
+            const cursor = stockCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
+        // GET 
+        app.get('/stock/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await stockCollection.findOne(query);
+            res.send(result);
+        });
+
+        // POST 
+        app.post('/stock', async (req, res) => {
+            const newStock = req.body;
+            const result = await stockCollection.insertOne(newStock);
+            res.send(result);
+        });
+
+        // Delete 
+        app.delete('/stock/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await stockCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+    }
+    finally { }
+
+}
+run().catch(console.dir)
 
 
 
